49bcedc9877faa0ef29fe4363f5ab0804400ddc1
singuerinc <nahuel.scotti@gmail.com>
2018-09-15 02:06:17 +0200

    test: word

---
+++ src/components/__tests__/Word.test.tsx
@@ -0,0 +1,20 @@
+import { shallow } from "enzyme";
+import * as React from "react";
+import { Word } from "../Word";
+
+describe("<Word />", () => {
+  describe("children", () => {
+    it("should contain a <h1 /> tag and text hello", () => {
+      const props = {
+        word: "hello",
+        className: "small"
+      };
+
+      const wrapper = shallow(<Word {...props} />);
+
+      expect(wrapper.find("h1")).toHaveLength(1);
+      expect(wrapper.text()).toBe("hello");
+      expect(wrapper.prop("className")).toBe("small");
+    });
+  });
+});
